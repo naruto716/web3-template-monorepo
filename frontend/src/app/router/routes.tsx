09d2aa65bcb1db99ac9ef9b13149ca966b446248
Isabel Body <isabelbody@gmail.com>
2025-03-22 12:44:43 +1300

    added new page

--- frontend/src/app/router/routes.tsx
+++ frontend/src/app/router/routes.tsx
@@ -3,6 +3,8 @@ import { RootLayout } from '../layout/RootLayout';
 import { HomePage } from '@/pages/HomePage';
 import { MarketplacePage } from '@/pages/MarketplacePage';
 import { ListItemPage } from '@/pages/ListItemPage';
+import { ItemDetailsPage } from '@/pages/ItemDetailsPage';
+import { createBrowserRouter } from 'react-router-dom';
 
 export const routes: RouteObject[] = [
   {
@@ -22,9 +24,32 @@ export const routes: RouteObject[] = [
         element: <ListItemPage />,
       },
       {
+        path: 'professional/:id',
+        element: <ItemDetailsPage />,
+      },
+      {
         path: '*',
         element: <Navigate to="/" replace />,
       },
     ],
   },
-]; 
+];
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomePage />,
+  },
+  {
+    path: '/marketplace',
+    element: <MarketplacePage />,
+  },
+  {
+    path: '/list-item',
+    element: <ListItemPage />,
+  },
+  {
+    path: '/professional/:id',
+    element: <ItemDetailsPage />,
+  },
+]); 
