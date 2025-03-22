2e173c269a237eaec0ea1bf507a9b9b0f9c889e3
yihanhhh <yguo193@aucklanduni.ac.nz>
2025-03-22 17:32:03 +1300

    Update test files

--- contracts/test/NewTalent.test.ts
+++ contracts/test/NewTalent.test.ts
@@ -5,10 +5,10 @@ describe("Talents", function() {
   it("should deploy successfully", async function() {
     const Talents = await ethers.getContractFactory("Talents");
     const talents = await Talents.deploy();
-    await talents.deployed();
+    await talents.deploymentTransaction()!.wait();
     
     // Basic test to verify deployment
-    expect(talents.address).to.be.properAddress;
+    expect(talents.target).to.be.properAddress;
   });
   
   // Add more tests as needed for your contract functionality
